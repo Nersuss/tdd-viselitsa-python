f27403a33328163af94618de2d9d5a299e3c8e0f
Maksim <nersus1337@mail.ru>
2025-04-04 16:42:15 +0700

    Доделал основную логику игры, получил рабочий прототип игры Виселица.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import random
+import random, os
 
+clear = lambda: os.system('cls')
 
 def str_is_valid(char):
     char = char.lower()
-    alphabet = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    alphabet = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
     if((len(char) == 1) and (char in alphabet)):
         return True
     return False
@@ -24,6 +25,134 @@ def char_contains_in_word(chars, word):
         return True
     return False
 
+def print_man(count_mistakes):
+    if count_mistakes == 0:
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('')
+        print('------------')
+    elif count_mistakes == 1:
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 2:
+        print('|-------')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 3:
+        print('|-------')
+        print('|      |')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 4:
+        print('|-------')
+        print('|      |')
+        print('|      0')
+        print('|')
+        print('|')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 5:
+        print('|-------')
+        print('|      |')
+        print('|      0')
+        print('|      |')
+        print('|      |')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 6:
+        print('|-------')
+        print('|      |')
+        print('|      0')
+        print('|     /|')
+        print('|      |')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 7:
+        print('|-------')
+        print('|      |')
+        print('|      0')
+        print('|     /|\\')
+        print('|      |')
+        print('|')
+        print('|')
+        print('------------')
+    elif count_mistakes == 8:
+        print('|-------')
+        print('|      |')
+        print('|      0')
+        print('|     /|\\')
+        print('|      |')
+        print('|     /')
+        print('|')
+        print('-----------')
+    elif count_mistakes == 9:
+        print('|-------')
+        print('|      |')
+        print('|      0')
+        print('|     /|\\')
+        print('|      |')
+        print('|     / \\')
+        print('|')
+        print('-----------')
+
+open_chars = []
+count_mistakes = 0
+
+def print_word(open_chars, word):
+    print('Слово: ', end='')
+    for i in range(0, len(word)):
+        if open_chars.__contains__(word[i]) :
+            print(word[i], end='')
+        else:
+            print('_', end='')
+    print()
+
+def is_end_game(open_chars, word):
+    for i in range(0, len(word)):
+        if not open_chars.__contains__(word[i]):
+            return False
+    return True
+
 if __name__ == '__main__':
-    print('Игра Виселица началась!')
-    print('Введите букву:')
+    print('Игра в Виселицу началась!')
+    while count_mistakes < 9 and is_end_game(open_chars, random_word) == False:
+        print_man(count_mistakes)
+        print_word(open_chars, random_word)
+
+        char = input('Введите букву:')
+        while ((str_is_valid(char) == False) or (open_chars.__contains__(char))):
+            print('Введен некорректный символ')
+            char = input('Введите букву:')
+        clear()
+        if char_contains_in_word(char, random_word):
+            open_chars.append(char)
+            print('Вы угадали букву!')
+        else:
+            count_mistakes+=1
+            print('Вы не угадали букву ;(')
+    if count_mistakes < 9:
+        print('Поздравляем, вы победили!')
+    else:
+        print('Вы проиграли ;(')

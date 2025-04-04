2b738bef2a083f2f18913e7b66c635dd1c014ec3
Maksim <nersus1337@mail.ru>
2025-04-04 12:24:01 +0700

    Создал соответствую тесту функцию для валидации строк.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+def str_is_valid(char):
+    char = char.lower()
+    alphabet = set('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    if((len(char) == 1) and (char in alphabet)):
+        return True
+    return False
+
 if __name__ == '__main__':
     print('PyCharm')
